ade4ab2b7d826a4aa7958fbf13bb9ba41a6443bd
Jason Gwartz <jason@gwartz.me>
2024-03-10 13:42:13 +0000

    First implementation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Create a GitHub Action Using TypeScript
+# `setup-pkl` GitHub Action
 
 [![GitHub Super-Linter](https://github.com/actions/typescript-action/actions/workflows/linter.yml/badge.svg)](https://github.com/super-linter/super-linter)
 ![CI](https://github.com/actions/typescript-action/actions/workflows/ci.yml/badge.svg)
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as gh from '@actions/github'
+import * as tc from '@actions/tool-cache'
 
 /**
  * The main function for the action.
@@ -7,18 +8,42 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const pklVersion = core.getInput('pkl-version')
+    const githubToken = core.getInput('github-token')
+
+    if (!githubToken) {
+      throw new Error('Input "github-token" is required')
+    }
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`Installing Pkl version ${pklVersion}`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const octokit = gh.getOctokit(githubToken)
+    const release = await octokit.rest.repos.getReleaseByTag({
+      owner: 'apple',
+      repo: 'pkl',
+      tag: `v${pklVersion}`
+    })
+    const assetId = release.data.assets.find(
+      a => a.name === 'pkl-linux-amd64'
+    )?.id
+    if (!assetId) {
+      throw new Error(`Unable to locate release for Pkl version: ${pklVersion}`)
+    }
+    const asset = await octokit.rest.repos.getReleaseAsset({
+      owner: 'apple',
+      repo: 'pkl',
+      asset_id: assetId
+    })
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const pklBinaryPath = await tc.downloadTool(asset.data.url)
+    const pklBinaryExtractedFolder = await tc.extractTar(pklBinaryPath)
+    const cachedPath = await tc.cacheDir(
+      pklBinaryExtractedFolder,
+      'pkl',
+      pklVersion
+    )
+    core.addPath(cachedPath)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
